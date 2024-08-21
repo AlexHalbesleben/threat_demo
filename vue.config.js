25e649fe912018c72de7bfc96182bd4f9eb412b6
Alex Halbesleben <81814191+AlexHalbesleben@users.noreply.github.com>
2024-08-21 16:34:38 -0500

    dist

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-module.exports = defineConfig({
-  transpileDependencies: true,
-  configureWebpack: {
-    plugins: [new BundleAnalyzerPlugin()],
-  },
-});
